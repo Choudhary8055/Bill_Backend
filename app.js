bb53c33af265b823aab33defa0014d8f004ac23f
Choudhary8055 <rahulchoudhary200599@gmail.com>
2022-09-29 12:31:16 +0530

    new

--- app.js
+++ app.js
@@ -11,84 +11,7 @@ app.use(cors());
 app.use(require('./routes/lable'));
 
 
-//Labels Api
 
-//Update Api
-
-// app.put('/update/:id', async (req, res) => {
-// 	try {
-// 		const { id } = req.params;
-// 		const data = await Label.findOne({ _id: id });
-// 		let oldDataResponse;
-// 		if (data) {
-// 			const olddata = new preData({
-// 				lable: data.lable,
-// 				amount: data.amount,
-// 				updatefrom: id
-// 			});
-
-// 			oldDataResponse = await olddata.save();
-// 		}
-
-// 		const updated = await Label.findByIdAndUpdate(id, req.body, { new: true });
-
-// 		res.status(200).json({ success: true, updated, oldDataResponse });
-// 	} catch (error) {
-// 		console.log(error);
-// 		res.status(400).json(error);
-// 	}
-// });
-
-// //Search Api
-//searchByLable
-// app.get('/serach/:lable', async (req, res) => {
-// 	try {
-// 		const { lable } = req.params;
-
-// 		const searchData = await Label.find({ lable });
-
-// 		res.status(200).json({ success: true, searchData });
-// 	} catch (error) {
-// 		console.log(error);
-// 		res.status(400).json(error);
-// 	}
-// });
-
-
-// app.get('/getlable', async (req, res) => {
-// 	try {
-// 		const data = await Label.find();
-// 		res.status(200).json(data);
-// 	} catch (error) {
-// 		console.log(error);
-// 		res.status(400).json(error);
-// 	}
-// });
-
-// app.get('/getolddata', async (req, res) => {
-// 	try {
-// 		const data = await preData.find();
-// 		res.status(200).json(data);
-// 	} catch (error) {
-// 		console.log(error);
-// 		res.status(400).json(error);
-// 	}
-// });  
-
-// app.get('/getDataById/:id', async (req, res) => {
-// 	try {
-// 		const { id } = req.params;
-
-// 		const data = await Label.findOne({ _id: id });
-// 		res.status(200).json({
-// 			success: true,
-// 			data
-// 		});
-// 	} catch (error) {
-// 		console.log(error);
-// 		res.status(400).json(error);
-// 	}
-// });
 
 // // app.delete('/delete', async (req, res) => {
 // // 	try {
@@ -101,16 +24,5 @@ app.use(require('./routes/lable'));
 // // 	}
 // // });
 
-// app.get('/serachbydate/:date', async (req, res) => {
-// 	try {
-// 		const { date } = req.params;
 
-// 		const searchData = await Label.find({ createdAt: date });
-
-// 		res.status(200).json({ success: true, searchData });
-// 	} catch (error) {
-// 		console.log(error);
-// 		res.status(400).json(error);
-// 	}
-// });
 app.listen(3500);
